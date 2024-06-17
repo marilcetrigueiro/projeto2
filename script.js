1e4275c486da576dcebcba3b19994ed3ad2f1bed
marilcetrigueiro <110105677+marilcetrigueiro@users.noreply.github.com>
2024-06-17 15:21:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-
+{
         enunciado: "Pergunta 1",
         alternativas: [
             "Alternativa 1",
@@ -17,3 +17,20 @@ const perguntas = [
         alternativas: [
             "Alternativa 1",
             "Alternativa 2"
+        ]
+    }
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+             "Alternativa 1",
+             "Alternativa 2"
+        ]
+    }
+    {
+         enunciado: "Pergunta 2",
+         alternativas: [
+             "Alternativa 1",
+             "Alternativa 2"
+        ]
+    }
+    {
